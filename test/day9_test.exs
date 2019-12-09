f8165945abb05c78b5b0b8cce66f40d4975a9303
Fabrice Nourisson <fnourisson@servebox.com>
2019-12-09 22:28:43 +0100

    WIP

--- test/day9_test.exs
+++ test/day9_test.exs
@@ -4,14 +4,14 @@ defmodule IntcodeTest do
 
   describe "day 9" do
     test "part 1" do
-      check_part1([{:output, 1219070632396864},{:value, 1219070632396864}],"1102,34915192,34915192,7,4,7,99,0")
-      check_part1([{:output, 1125899906842624},{:value, 1125899906842624}],  "104,1125899906842624,99")
+      check_part1([{:output, 1219070632396864},{:state, :stop}],"1102,34915192,34915192,7,4,7,99,0")
+      check_part1([{:output, 1125899906842624},{:state, :stop}],  "104,1125899906842624,99")
       expected = [{:output, 109}, {:output, 1}, {:output, 204},
                   {:output, -1}, {:output, 1001}, {:output, 100},
                   {:output, 1}, {:output, 100}, {:output, 1008},
                   {:output, 100}, {:output, 16}, {:output, 101},
                   {:output, 1006}, {:output, 101}, {:output, 0}, {:output, 99},
-                  {:value, 99}
+                  {:state, :stop}
                  ]
       input = "109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99"
       check_part1(expected, input)
