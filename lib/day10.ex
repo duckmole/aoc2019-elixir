c5a59986a35d7675203ef371dd641214343e3f6f
Fabrice Nourisson <fnourisson@servebox.com>
2019-12-10 18:36:36 +0100

    Day 10

--- lib/day10.ex
+++ lib/day10.ex
@@ -17,20 +17,44 @@ defmodule Day10 do
         end))
   end
 
+  def run_part2(map) do
+    {{x_origin, y_origin}, _} = Day10.station_asteroids(map)
+    v = vectors(map)
+    {x,y} = parse(Enum.reverse(Enum.sort(Map.keys(v))), v, 1)
+    100*(x_origin+x)+(y_origin-y)
+  end
+
+  def parse([], v, index), do: parse(Enum.reverse(Enum.sort(Map.keys(v))), v, index)
+  def parse([key| tail], v, 199) do
+    [{x,y}| asteroids] = v[key]
+    {x,y}
+  end
+  def parse([key| tail_key], v, index) do
+    [{x,y}| asteroids] = v[key]
+    new_v = case asteroids do
+              [] ->
+                Map.drop(v, [key])
+                _ -> %{v | key => asteroids}
+              end
+    parse(tail_key, new_v, index+1)
+  end
+
   def station_asteroids(map) do
     {List.first(asteroids(String.split(map), 0, [], "X")), asteroids(String.split(map), 0, [], "#")}
   end
 
-  def vector(map) do
+  def vectors(map) do
     { {x_base, y_base } , asteroids } = station_asteroids(map)
-    Enum.map(asteroids, fn({x,y} ->
-      case y do
-        0 -> {{x/abs(x), 1000}, {x_base - x, y_base - y }}
-        _ -> {{x/abs(x), x/y}, {x_base - x, y_base - y }}
+    result = Enum.map(asteroids, fn({x,y}) ->
+      vector = {v_x, v_y} = {x - x_base, y_base - y }
+      case  v_x do
+        0 -> {{v_x >= 0 , 1000}, vector}
+        _ -> {{v_x >= 0, v_y/v_x}, vector}
       end
-    )
-
     end
+    )
+    Enum.group_by(result, fn({x,y}) -> x end, fn({x,y}) -> y end )
+  end
 
   def asteroids(map) do
     asteroids(String.split(map), 0, [], "#")
@@ -43,9 +67,7 @@ defmodule Day10 do
     asteroids(tail, line_nb + 1, asteroids_in_line(String.split(line, ""), line_nb, -1, [], pattern)  ++ acc,
       pattern)
   end
-  def asteroids_in_line([], _, _, acc, pattern) do
-    acc
-  end
+  def asteroids_in_line([], _, _, acc, pattern), do: acc
   def asteroids_in_line([pattern|tail], line_nb, col_nb, acc, pattern) do
     asteroids_in_line(tail, line_nb, col_nb+1, [{col_nb, line_nb} | acc], pattern)
   end
@@ -73,7 +95,7 @@ defmodule Day10 do
           y = y_origin - step_y * n
           case {round(x) == x, round(y) == y} do
             {true, true} ->
-              Enum.member?(asteroid_map, {round(x), round(y)})
+              Enum.member?(asteroid_map, {round(x), round(y)}
             _ -> false
           end
         end) == []
